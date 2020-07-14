d886199dd716d0abebb029c9b5b460465d68adc9
Vladislav1303 <62154009+Vladislav1303@users.noreply.github.com>
2020-07-14 21:23:34 +0300

    Add files via upload

---
+++ precache-manifest.cce71af602633aa7dc52552e8ee25ed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6472ec7e095db4c294b5579ebfa7b8e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920a869d4c42e5f42b31",
+    "url": "/static/css/2.a6674286.chunk.css"
+  },
+  {
+    "revision": "eaf35ca0853e9a73f6d5",
+    "url": "/static/css/main.823ed6fe.chunk.css"
+  },
+  {
+    "revision": "920a869d4c42e5f42b31",
+    "url": "/static/js/2.8b3a0997.chunk.js"
+  },
+  {
+    "revision": "acff744c5c5e21e2b14a11e29451820a",
+    "url": "/static/js/2.8b3a0997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf35ca0853e9a73f6d5",
+    "url": "/static/js/main.181337b6.chunk.js"
+  },
+  {
+    "revision": "65b16d389ad0d67af2cd",
+    "url": "/static/js/runtime-main.2c360d01.js"
+  }
+]);
